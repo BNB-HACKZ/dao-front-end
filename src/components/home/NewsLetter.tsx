fc573f3573e99910c81851bbec2a1080d31f860d
Frank_Udoags <frankudoags@gmail.com>
2023-04-30 11:29:34 +0100

    Works

--- src/components/home/NewsLetter.tsx
+++ src/components/home/NewsLetter.tsx
@@ -5,10 +5,10 @@ const NewsLetter = () => {
     <div className="bg-[#71b15d] w-full">
       <div className="w-full max-w-7xl py-12 mx-auto flex flex-col lg:flex-row gap-8 px-6 min-h-[40vh] lg:h-[40vh]">
         <div>
-          <h1 className="text-white text-5xl font-medium">
+          <h1 className="text-white text-2xl lg:text-5xl font-medium">
             Subscribe to our newsletter
           </h1>
-          <p className="text-white text-lg pt-4 font-light">
+          <p className="text-white lg:text-lg pt-4 font-light">
             Leave us with your email and never miss a single update.
           </p>
           {/* input and suscribe, side by side */}
