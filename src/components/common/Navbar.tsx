1da8827bed8ae07be3c8d18b69b57ee029bc7a79
Frank_Udoags <frankudoags@gmail.com>
2023-05-13 18:24:11 +0100

    works

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -35,7 +35,9 @@ export default function Navbar() {
             >
               JOIN OUR DAO
             </Link>
-            <ConnectButton />
+            <ConnectButton
+              chainStatus="icon"
+            />
           </Popover.Group>
           <div className="-my-2 -mr-2 md:hidden">
             <Popover.Button className="inline-flex items-center justify-center rounded-md text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black">
