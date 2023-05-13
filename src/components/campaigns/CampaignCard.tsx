f3fd469ea1d45a17beff663f663d273d7306744c
Frank_Udoags <frankudoags@gmail.com>
2023-05-13 12:09:29 +0100

    works

--- src/components/campaigns/CampaignCard.tsx
+++ src/components/campaigns/CampaignCard.tsx
@@ -13,7 +13,10 @@ interface CampaignCardProps {
 
 const CampaignCard = ({ campaign }: CampaignCardProps) => {
   return (
-    <div className="cursor-pointer flex flex-col items-center justify-start w-full h-full p-4 rounded-2xl shadow-md border-2 border-[#161a28]">
+    <Link
+      href={"campaign/1"}
+      className="cursor-pointer flex flex-col items-center justify-start w-full h-full p-4 rounded-2xl shadow-md border-2 border-[#161a28]"
+    >
       <div className="w-full h-[250px] rounded-lg overflow-hidden">
         <Image
           src={campaign.image}
@@ -22,14 +25,16 @@ const CampaignCard = ({ campaign }: CampaignCardProps) => {
         />
       </div>
       <div className="flex flex-col items-start justify-center w-full">
-        <h3 className="text-lg font-medium text-white max-w-[300px] py-6">{campaign.title}</h3>
+        <h3 className="text-lg font-medium text-white max-w-[300px] py-6">
+          {campaign.title}
+        </h3>
         <span>Raising</span>
         <div className="flex gap-1 items-center justify-start w-full h-8 text-lime">
           <span className="font-semibold">{campaign.amount}</span>
           <span className="font-semibold">{campaign.currency}</span>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
