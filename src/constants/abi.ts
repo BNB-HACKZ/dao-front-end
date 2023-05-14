ae0354b96ef53adfe41e64f54fe2d9f132a72de9
Frank_Udoags <frankudoags@gmail.com>
2023-05-14 15:34:14 +0100

    constants

---
+++ src/constants/abi.ts
@@ -0,0 +1,12 @@
+export const ABI = {
+  //CAMPAIGN ABI
+  campaign: [],
+  //TOKEN ABI
+  token: [],
+  //SBT ABI
+  sbt: [],
+  //CAMPAIGN FACTORY ABI
+  campaignFactory: [],
+  //DAO ABI
+  dao: [],
+};
